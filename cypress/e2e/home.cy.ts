25e41d46053cf0380e4a4a1f9525343f9df3b8a3
satang <117140785+littleyeeraf@users.noreply.github.com>
2023-07-07 10:14:35 +0700

    Add test for course section

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -16,4 +16,21 @@ describe('home page', () => {
       cy.get('dt').eq(2).contains('Free and Open Source')
     })
   })
+
+  context('Courses section', () => {
+    it('Course: Testing Your First Next.js Application', () => {
+      cy.getByData('course-0').find('a').contains('Get started').click()
+      cy.location('pathname').should('equal', '/testing-your-first-application')
+    })
+
+    it('Course: Testing Foundations', () => {
+      cy.getByData('course-1').find('a').contains('Get started').click()
+      cy.location('pathname').should('equal', '/testing-foundations')
+    })
+
+    it('Course: Cypress Fundamentals', () => {
+      cy.getByData('course-2').find('a').contains('Get started').click()
+      cy.location('pathname').should('equal', '/cypress-fundamentals')
+    })
+  })
 })
